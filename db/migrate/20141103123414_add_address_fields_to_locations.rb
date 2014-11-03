757fdc9a7f3cc0a4608fc82e02d6616a1c9268ff
Estela <estelapedrero@gmail.com>
2014-11-03 13:53:37 +0100

    first db

---
+++ db/migrate/20141103123414_add_address_fields_to_locations.rb
@@ -0,0 +1,8 @@
+class AddAddressFieldsToLocations < ActiveRecord::Migration
+  def change
+  	add_column :locations, :zip_code, :integer
+  	add_column :locations, :street, :string
+  	add_column :locations, :country, :string
+  	add_column :locations, :description, :text
+  end
+end
